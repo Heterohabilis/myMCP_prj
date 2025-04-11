87d61740c213ef5bc8aba3600e81ca2207f7e1f6
Heterohabilis <mutong221@gmail.com>
2025-04-11 02:54:33 -0400

    edited mcp_server.yaml

--- test_servers/bash_runner.py
+++ test_servers/bash_runner.py
@@ -9,7 +9,7 @@ DESCRIPTION = '''[Generate a valid bash script] that solves the user’s task.
 - Return your response as plain bash commands, separated by newlines.  
 - when open an application, just use the correct name, ex: $ google-chrome
 - The output should be directly executable in a non-root Unix environment.  
-example: open an application, write code, find files...
+example: open an application, write code, find files.
 '''
 
 
